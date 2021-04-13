bd0362dda2965d19616b98d988e6b2c39448f12d
Ali, Muhammad <mali301@gatech.edu>
2021-04-12 21:53:08 -0600

    using hashrouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter basename ="/dataviz_mockup">
+  <HashRouter basename ="/dataviz_mockup">
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
